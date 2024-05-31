ac2aa1281717dafde59ecdc17b0b895d62b87ac4
Rafael Vale <rafaelangelopv@gmail.com>
2024-05-31 19:36:36 -0300

    update indentation

--- src/main.js
+++ src/main.js
@@ -5,59 +5,59 @@ Isaias Do Amaral Sousa / 2416767
 Rodrigo Fernandes Targino de Souza / 2415489
 Lucas Ramos Ribeiro / 2417012 */
 
-document.getElementById("btnIniciar").addEventListener("click", function() {
+document.getElementById("btnIniciar").addEventListener("click", function () {
     megaSena();
 });
 
-function megaSena(){
+function megaSena() {
     var numerosEscolhidos = [];
     var numerosSorteados = [];
 
     // Insira 6 números diferentes entre 1 e 60
-    for (var i = 0; i < 6; i++){
+    for (var i = 0; i < 6; i++) {
         var numero = parseInt(prompt("Insira um " + (i + 1) + " número entre 1 e 60:"));
 
         // Verifique se o número é válido e diferente dos anteriores
-        if (numero < 1 || numero > 60 || numerosEscolhidos.includes(numero)){
+        if (numero < 1 || numero > 60 || numerosEscolhidos.includes(numero)) {
             alert("Insira um número válido, diferente dos anteriores.");
             return;
         }
         numerosEscolhidos.push(numero);
     }
-        // Ordena os números escolhidos em ordem crescente
-        numerosEscolhidos.sort(function(a, b) {
+    // Ordena os números escolhidos em ordem crescente
+    numerosEscolhidos.sort(function (a, b) {
         return a - b;
-        });
+    });
 
     // Sorteie 6 números aleatórios entre 1 e 60
-    for (var i = 0; i < 6; i++){
+    for (var i = 0; i < 6; i++) {
         var numeroSorteado;
-        do{
+        do {
             numeroSorteado = Math.floor(Math.random() * 60) + 1;
         } while (numerosSorteados.includes(numeroSorteado));
         numerosSorteados.push(numeroSorteado);
     }
-    
-   // Compare os números e determine os acertos
+
+    // Compare os números e determine os acertos
     var acertos = 0;
-    for (var i = 0; i < 6; i++){
-        if (numerosEscolhidos.includes(numerosSorteados[i])){
+    for (var i = 0; i < 6; i++) {
+        if (numerosEscolhidos.includes(numerosSorteados[i])) {
             acertos++;
         }
     }
 
     // Mostre a mensagem de acordo com os acertos
-    if (acertos === 6){
+    if (acertos === 6) {
         alert("Parabéns! Você acertou a SENA e ganhou!");
-    } else if (acertos === 5){
+    } else if (acertos === 5) {
         alert("Parabéns! Você acertou a QUINA e ganhou!");
-    } else if (acertos === 4){
+    } else if (acertos === 4) {
         alert("Parabéns! Você acertou a QUADRA e ganhou!");
-    } else{
+    } else {
         alert("Infelizmente, você perdeu!");
     }
 
- // Mostre os números escolhidos e sorteados
+    // Mostre os números escolhidos e sorteados
     alert("Números escolhidos: " + numerosEscolhidos.join(", "));
     alert("Números sorteados: " + numerosSorteados.join(", "));
 }
